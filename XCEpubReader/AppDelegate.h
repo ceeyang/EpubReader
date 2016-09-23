c2825cdb988578e41f640a1228baf827795bbcb7
杨西川 <yangxichuan@zhixueyun.com>
2016-09-23 17:21:42 +0800

    Initial Commit

---
+++ XCEpubReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCEpubReader
+//
+//  Created by pro on 2016/9/23.
+//  Copyright © 2016年 daisy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCEpubReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCEpubReader
+//
+//  Created by pro on 2016/9/23.
+//  Copyright © 2016年 daisy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
